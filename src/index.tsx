c9bbea7273ce720f78fd8b552726a9c9f9a0a214
LuisFlahan4051 <luisflahan4051@gmail.com>
2022-06-12 23:22:48 -0500

    Migrated to vite

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.scss';
 import { ApolloProvider } from '@apollo/client';
 import { client, URIGRAPHQL } from './apollo/client'
 import Main from './components/Main';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,8 +15,3 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
