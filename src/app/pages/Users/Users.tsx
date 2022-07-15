684e8a20e5e9a0dd4b8caca77b99fad143bce3cc
luisflahan4051 <luisflahan4051@gmail.com>
2022-07-15 01:43:06 -0500

    Working at users form

--- src/app/pages/Users/Users.tsx
+++ src/app/pages/Users/Users.tsx
@@ -2,7 +2,24 @@ import './Users.scss'
 export default function Users() {
 	return (
 		<div className='Users'>
-			<h1>Users</h1>
+			<div className='users__find'>
+				<div className='find_username'>
+					<input className='username_input' type='text' placeholder='Usuario' />
+					<div className='username_buttons'>
+						<button className='username_back'></button>
+						<button className='username_next'></button>
+					</div>
+				</div>
+				<div className='find_selector'>
+					<div className='selector_users'></div>
+					<div className='selector_options'>
+						<div className='option_add-user'>Usuarios</div>
+					</div>
+				</div>
+			</div>
+			<div className='users__info'>
+				<div className='info_target'></div>
+			</div>
 		</div>
 	)
 }
